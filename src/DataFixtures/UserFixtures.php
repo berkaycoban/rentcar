66eef4e7b9c0bed0d4c7ac80038df412360f721a
Berkay COBAN <bcobann35@gmail.com>
2021-01-12 21:21:20 +0300

    :zap: Add Security, Fixtures bundles and Create User Entity

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    /**
+     * UserFixtures constructor.
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder){
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail("berkay@rentcar.com")
+            ->setPassword($this->passwordEncoder->encodePassword($user, '12345678'))
+            ->setRoles(["ROLE_SUPER_USER"])
+            ->setCreatedAt(new DateTime());
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
