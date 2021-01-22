f30aef055301367e140edebabf6767ffd2a1c169
Berkay COBAN <bcobann35@gmail.com>
2021-01-22 20:36:30 +0300

    :zap: Add TransactionType, create page and Update Controller, sidebar, index templates

--- src/Controller/Admin/TransactionController.php
+++ src/Controller/Admin/TransactionController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Transaction;
+use App\Form\TransactionType;
 use App\Repository\TransactionRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
 use Twig\Environment;
 
 /**
@@ -34,9 +39,59 @@ class TransactionController
      */
     public function index(TransactionRepository $repository): Response
     {
-        // sadece o sirkete ait transaction islemlerini alabiliriz...
+
         $transactions = $repository->findAll();
         $content = $this->twig->render('admin/transaction/index.html.twig', ['transactions' => $transactions]);
+
+        return new Response($content);
+    }
+
+    /**
+     * @Route("/create/{id}", name="create", defaults={"id": null})
+     * @param FormFactoryInterface $formFactory
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param TransactionRepository $repository
+     * @param Security $security
+     * @param null $id
+     * @return Response
+     */
+    public function create(
+        FormFactoryInterface $formFactory,
+        Request $request,
+        EntityManagerInterface $em,
+        TransactionRepository $repository,
+        Security $security,
+        $id = null
+    ): Response
+    {
+        $transaction = new Transaction();
+
+        if(!is_null($id) && ((int)$id) > 0) {
+            $transaction = $repository->find($id) ?? $transaction;
+        }
+
+        $company_id = $security->getUser()->getCompany()->getId();
+        $form = $formFactory->create(TransactionType::class, $transaction, [
+            'company_id' => $company_id
+        ]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $date = $form->get('reservationDate')->getData();
+            // tarig explode edilip
+            // ilk ve ikinci tarih bulunacak
+            // sonra gun hesaplanacak
+            // sonra fiyat, km hesaplanacak
+
+            // amount
+            // pickup km
+            // return km
+        }
+
+        $content = $this->twig->render('admin/transaction/create.html.twig', [
+            'form' => $form->createView()
+        ]);
 
         return new Response($content);
     }
@@ -52,7 +107,7 @@ class TransactionController
     {
         $transaction = $repository->find($id);
 
-        if($transaction){
+        if ($transaction) {
             $transaction->getCarId()->setAvailable(true);
             $em->remove($transaction);
             $em->flush();
---
+++ src/Form/TransactionType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use App\Entity\Transaction;
+use App\Entity\User;
+use App\Repository\CarRepository;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TransactionType extends AbstractType
+{
+    private $carRepository;
+    private $userRepository;
+
+    public function __construct(CarRepository $repo, UserRepository $user) {
+        $this->carRepository = $repo;
+        $this->userRepository = $user;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('car_id', EntityType::class, [
+                'class' => Car::class,
+                'label' => 'Car',
+                'choices' => $this->carRepository->findAllCarsByCompany($options['company_id'])
+            ])
+            ->add('customer_id', EntityType::class, [
+                'class' => User::class,
+                'label' => 'Customer',
+                'choices' => $this->userRepository->findAllCustomers(),
+                'choice_label' => function ($user) {
+                    return $user->getFullName();
+                }
+            ])
+            ->add('reservationDate', TextType::class, [
+                'mapped' => false,
+                'required' => true,
+                'label' => 'Pickup and Return time range:'
+            ])
+
+            ->add('save', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Transaction::class,
+            'company_id' => 1
+        ]);
+
+        $resolver->setAllowedTypes('company_id', 'int');
+    }
+}
