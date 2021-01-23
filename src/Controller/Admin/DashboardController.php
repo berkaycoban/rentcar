ea0aeb9df925b1264bf384a4fe38fd75e198d3a6
Berkay COBAN <bcobann35@gmail.com>
2021-01-23 15:35:34 +0300

    :zap: Update DashboardController

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,22 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
 
 /**
  * @Route("/admin", name="app_admin_")
  */
-class DashboardController extends AbstractController
+class DashboardController
 {
+    private $twig;
+    private $router;
+    private $flash;
+
+    public function __construct(Environment $twig, RouterInterface $router, FlashBagInterface $flash){
+        $this->twig = $twig;
+        $this->router = $router;
+        $this->flash = $flash;
+    }
+
     /**
      * @Route("/", name="dashboard")
+     * @return Response
      */
     public function index(): Response
     {
-        return $this->render('admin/dashboard/index.html.twig', [
+        $content = $this->twig->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
+
+        return new Response($content);
     }
 }
